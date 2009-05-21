ce5b542a6c1c8c340258695e46a87a07b4ef1463
thanhnga86 <thanhnga86@ebc52662-056d-11de-b5bf-434079a3519f>
2009-05-21 10:13:23 +0000

    GOI EXE TU JAVA RUNTIME

---
+++ trunk/Documents/call.java
@@ -0,0 +1,42 @@
+<xmp>UNKNOWN
+
+//**************************************
+//HTML for :Call exe from java
+//**************************************
+public class Main {
+
+    {
+    	public static void main(String[] args) 
+
+        	{
+        		try
+
+            		{
+            			Runtime rt=Runtime.getRuntime();
+            			
+            			//(A)	Calling a html from Java
+            			
+            			//1St Way
+            				rt.exec("C:/consonle.exe");
+            		
+            			/* 2nd Way
+            					
+            						String[] cmd = new String[4];
+            						cmd[0] = "cmd.exe";
+            						cmd[1] = "/C";
+            						cmd[2] = "start";
+            						cmd[3] = "C://j2sdk1.4.2_03//bin//nil.html";
+            			*/
+            			//(B) Calling an .exe from java
+            			rt.exec("C:/consonle.exe");
+            		}
+            		catch(Throwable t)
+
+                		{
+                			System.out.print(t.getMessage());
+                		
+                		}
+                	}
+            }
+	}	
+		

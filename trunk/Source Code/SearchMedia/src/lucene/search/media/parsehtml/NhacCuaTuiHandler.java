6d94c9d55442ef90937b93b7994f5756863dadf0
thanhnga86 <thanhnga86@ebc52662-056d-11de-b5bf-434079a3519f>
2009-05-28 18:09:58 +0000

    them ham getComent trong file HtmlHandler.java

--- trunk/Source Code/SearchMedia/src/lucene/search/media/parsehtml/NhacCuaTuiHandler.java
+++ trunk/Source Code/SearchMedia/src/lucene/search/media/parsehtml/NhacCuaTuiHandler.java
@@ -6,10 +6,7 @@
 package lucene.search.media.parsehtml;
 
 import org.apache.lucene.document.Document;
-import org.apache.lucene.document.Field;
-
 import org.cyberneko.html.parsers.DOMFragmentParser;
-
 import org.xml.sax.SAXException;
 import org.xml.sax.InputSource;
 import org.w3c.dom.DocumentFragment;
@@ -22,11 +19,12 @@ import lucene.search.media.indexer.Operators;
 import lucene.search.media.objects.MediaObject;
 import lucene.search.media.parseframework.*;
 
+
 /**
  *
  * @author Administrator
  */
-public class NhacCuaTuiHandler extends HtmlHandler implements DocumentHandler {
+public class NhacCuaTuiHandler extends HtmlHandler  implements DocumentHandler {
 private DOMFragmentParser parser = new DOMFragmentParser();
 
   public Document getDocument(InputStream is)
@@ -50,22 +48,29 @@ private DOMFragmentParser parser = new DOMFragmentParser();
       new org.apache.lucene.document.Document();
 
    
-       StringBuffer sb = new StringBuffer();
-   // HtmlHandler html=new HtmlHandler();
+   StringBuffer sb = new StringBuffer();
+    MediaObject obj=new MediaObject();
+   
+       //get source link(real link on website)
+    //sb contains array comment and only first item in this array  is available
+    getComment(sb, node, 1);
+    if(sb!=null)
+        obj.setLinksource(AnalysisComment(sb.toString()));
+    sb=new StringBuffer();
     getText(sb, node, "title",1);
     String title = sb.toString();
       //get object
       sb=new StringBuffer();
     //note :link in nhaccuatui is sourcelink not is a media link
     StringBuffer link=new StringBuffer();
-    getObjects(sb, node, "object",1,false,true,link);
+    getObjects(sb, node, "object",2,false,true,link);
 
     if (title != null && !sb.toString().equals("")){
-   
-     MediaObject obj=new MediaObject();
+
+
        obj.setLinkobject(sb.toString());
     //analysis the title
-        String[]Str=AnalysisTitle(title);
+       String[]Str=AnalysisTitle(title);
        obj.setLinkobject(sb.toString());
        obj.setLinkmedia(link.toString());
     for(int i=0;i<Str.length;i++)
@@ -78,15 +83,11 @@ private DOMFragmentParser parser = new DOMFragmentParser();
          obj.setSongen(Str[i]);
         else if(i==3)//first is name song with UTF-8 encode
          obj.setSingeren(Str[i]);
-    }     
+    }
         //only index object obtain link media if this page has title(name of media)
-        //get source link(real link on website)
-        sb=new StringBuffer();
-        getText(sb, node, "httrack",1);
-        if(sb!=null)
-            obj.setLinksource(sb.toString());  
-       
-       
+
+
+
 
 //        //get singer
 //        sb=new StringBuffer();
@@ -107,9 +108,11 @@ private DOMFragmentParser parser = new DOMFragmentParser();
        Operators Op=new Operators();
        doc=Op.addDocumentObject(obj);
     }
+   
     return doc;
 
   }
+ 
 
   public static void main(String args[]) throws Exception {
     NhacCuaTuiHandler handler = new NhacCuaTuiHandler();
@@ -117,19 +120,19 @@ private DOMFragmentParser parser = new DOMFragmentParser();
       new FileInputStream(new File(args[0])));
    // while(doc.fields().hasMoreElements()){
     //System.out.println(doc.get("url"));
-    if(doc!=null){
-        System.out.println(doc.getField("songvn").stringValue());
-        System.out.println(doc.getField("songen").stringValue());
-        System.out.println(doc.getField("singervn").stringValue());
-        System.out.println(doc.getField("singeren").stringValue());
-        System.out.println(doc.getField("linkobject").stringValue());
-        System.out.println(doc.getField("linksource").stringValue());
-        System.out.println(doc.getField("linkmedia").stringValue());
-        System.out.println(doc.getField("albumen").stringValue());
-        System.out.println(doc.getField("albumvn").stringValue());
+//    if(doc!=null){
+//        System.out.println(doc.getField("songvn").stringValue());
+//        System.out.println(doc.getField("songen").stringValue());
+//        System.out.println(doc.getField("singervn").stringValue());
+//        System.out.println(doc.getField("singeren").stringValue());
+//        System.out.println(doc.getField("linkobject").stringValue());
+//        System.out.println(doc.getField("linksource").stringValue());
+//        System.out.println(doc.getField("linkmedia").stringValue());
+//        System.out.println(doc.getField("albumen").stringValue());
+//        System.out.println(doc.getField("albumvn").stringValue());
 
     //doc.fields().nextElement();
   //}
-    }
+  //  }
   }
 }
